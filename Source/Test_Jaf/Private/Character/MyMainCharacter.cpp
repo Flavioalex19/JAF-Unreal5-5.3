d2ff6ae8081b646c9a09fbd9267ec4a4c98a981c
FA77\flavi <flavioalex19@gmail.com>
2023-10-13 22:06:02 -0300

    Scripts-Character, Input Actions and Mapping

---
+++ Source/Test_Jaf/Private/Character/MyMainCharacter.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/MyMainCharacter.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMyMainCharacter::AMyMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("View Camera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void AMyMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			
+			Subsystem->AddMappingContext(MyMapping, 0);
+		}
+	}
+	
+}
+
+void AMyMainCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2d MovementValue = Value.Get<FVector2d>();
+	const FRotator Rotation = GetController()->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetScaledAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementValue.Y);
+	
+	const FVector Right = FRotationMatrix(YawRotation).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Right, MovementValue.X);
+}
+
+void AMyMainCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2d LookAxisVector = Value.Get<FVector2d>();
+
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+}
+
+// Called every frame
+void AMyMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyMainCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMyMainCharacter::Look);
+	}
+
+}
+
---
+++ Source/Test_Jaf/Public/Character/MyMainCharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "GameFramework/Character.h"
+#include "MyMainCharacter.generated.h"
+
+UCLASS()
+class TEST_JAF_API AMyMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Input)
+	class UInputMappingContext* MyMapping;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Input)
+	class UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Input)
+	class UInputAction* LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* CameraBoom;
+	class UCameraComponent* ViewCamera;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+};
